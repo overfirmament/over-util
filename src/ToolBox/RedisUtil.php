d855dedf56984f80d7c1dceeed3400f7426fbe76
刘学之 <liuxuezhi@liuxuezhideMacBook-Pro.local>
2025-03-04 16:17:03 +0800

    update

--- src/ToolBox/RedisUtil.php
+++ src/ToolBox/RedisUtil.php
@@ -4,47 +4,107 @@ namespace Overfirmament\OverUtils\ToolBox;
 
 use Illuminate\Support\Facades\Redis;
 use Illuminate\Redis\Connections\Connection;
+use Illuminate\Support\Str;
 
-class RedisUtil
+/**
+ * @method keys(string $key)
+ * @method get(string $key)
+ * @method set(string $key, string $value)
+ * @method setNx(string $key, string $value): int
+ * @method bool exists(string $key)
+ * @method incr(string $key)
+ * @method setEx(string $key, int $ttl, string $value)
+ * @method zAdd(string $key, ...$scoreAndMember)
+ * @method zRangeByScore(string $key, int $min, int $max)
+ * @method zRangeByScoreWithScores(string $key, int $min, int $max)
+ * @method zRange(string $key, int $start, int $stop)
+ * @method zRangeWithScores(string $key, int $start, int $stop)
+ * @method zRemRangeByScore(string $key, $min, $max)
+ * @method zRemRangeByRank(string $key, $start, $end)
+ * @method zCard(string $key)
+ * @method pipeline(callable $callback = null)
+ * @method mGet(array $key)
+ * @method mSet(...$key)
+ * @method publish(string $key, string $value)
+ * @method expire(string $key, int $seconds)
+ * @method int hDel(string $key, ...$fields)
+ * @method hGet(string $key, string $field)
+ * @method hSet(string $key, string $field, string $value)
+ * @method hMGet(string $key, ...$fields)
+ * @method hMSet(string $key, ...$fieldAndValue)
+ * @method array hGetAll(string $key)
+ * @method hKeys(string $key)
+ * @method hVals(string $key)
+ * @method sAdd(string $key, ...$members)
+ * @method array sMembers(string $key)
+ * @method incrBy(string $key, int $value)
+ * @method zScore(string $key, string $member)
+ * @method zIncrBy(string $key, int $increment, string $member)
+ * @method zRem(string $key, ...$members)
+ */
+class RedisUtil extends Redis
 {
+    protected static $instances = [];
+
     protected Connection $redis;
 
 
-    public static function init($connection = null): RedisUtil
+    /**
+     * 初始化
+     *
+     * @param  string  $connection
+     *
+     * @return RedisUtil
+     */
+    public static function init(string $connection = 'default'): RedisUtil
     {
-        $redisUtil = new self();
-        $connection = $connection ?? "default";
-        $redisUtil->redis = Redis::connection($connection);
-        return $redisUtil;
+        return static::$instances[$connection] ??= tap(new self(), function ($redisUtil) use ($connection) {
+            $redisUtil->redis = Redis::connection($connection);
+        });
+    }
+
+
+
+    /**
+     * 返回具体的key值
+     *
+     * @param  $patternKeys
+     *
+     * @return array
+     */
+    public function patternKeys(...$patternKeys): array
+    {
+        $keys = $this->redis->pipeline(function ($pipe) use ($patternKeys) {
+            foreach ($patternKeys as $key) {
+                 $pipe->keys($key);
+            }
+        });
+
+        return \Arr::collapse($keys);
     }
 
 
     /**
-     * @param  string  $key
-     * @param  array  $data
-     * @param  int  $ttl
+     * @param $key
      *
-     * @return mixed
+     * @return int
      */
-    public function hmset(string $key, array $data, int $ttl = 0): mixed
+    public function del($key): int
     {
-        $hash = [];
-        foreach ($data as $k => $v) {
-            $hash[] = $k;
-            $hash[] = $v;
+        if (filled($key)) {
+            return $this->redis->del($key);
+        } else {
+            return 0;
         }
-        $res = $this->redis->hmset($key, ...$hash);
-
-        if ($ttl) {
-            $this->redis->expire($key, $ttl);
-        }
-
-        return $res;
     }
 
 
-    public function __call($method, $args)
+    public function __call($name, $arguments)
     {
-        return $this->redis->$method($args);
+        if (\Str::endsWith($name, "WithScores")) {
+            $name = \Str::before($name, "WithScores");
+            $arguments[] = ['WITHSCORES' => true];
+        }
+        return $this->redis->$name(...$arguments);
     }
 }
